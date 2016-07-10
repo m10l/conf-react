599f4ff39a7d33b1c0af74be78f778b287fda34d
Mike Mitchell <mikemitchell@Mikes-MacBook-Air.local>
2016-07-10 15:17:50 +0100

    Added most recently available data, sorted by newest first

--- src/components/PostListComponent.js
+++ src/components/PostListComponent.js
@@ -1,94 +1,72 @@
 'use strict';
 
 import React from 'react';
+import _ from 'lodash';
 import PostListFilterComponent from 'components/PostListFilterComponent';
 import PostComponent from 'components/PostComponent';
 
 require('styles/PostList.scss');
 
 class PostListComponent extends React.Component {
-  render() {
-    var postsArray = [
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': 2,
-        'updated': 1429566400826,
-        '__v': 0
-      },
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': 2,
-        'updated': 1429566402608,
-        '__v': 0
-      },
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': 4,
-        'updated': 1429566409761,
-        '__v': 0
-      },
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': 1,
-        'updated': 1429566452774,
-        '__v': 0
-      },
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': 0,
-        'updated': 1429566465165,
-        '__v': 0
-      },
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': 2,
-        'updated': 1429566467349,
-        '__v': 0
-      },
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': -2,
-        'updated': 1429566469165,
-        '__v': 0
-      },
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': -2,
-        'updated': 1429566474210,
-        '__v': 0
-      },
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': -2,
-        'updated': 1429566476493,
-        '__v': 0
-      },
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': 10,
-        'updated': 1429566480274,
-        '__v': 0
-      },
-      {
-        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
-        'from': 'test',
-        'votes': 14,
-        'updated': 1429566482417,
-        '__v': 0
-      }
-    ];
+  constructor() {
+    super();
+    this.state = {
+      data: _.orderBy([
+        {
+          '_id': '5530f9140857b80300388c6e',
+          '__v': 0,
+          'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+          'updated': '2015-04-20T22:26:29.399Z',
+          'votes': 23
+        },
+        {
+          '_id': '5530fb5e0857b80300388c6f',
+          '__v': 0,
+          'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/RE5e147fbe4eb9a91a19de414a9a3b2bbf',
+          'updated': '2015-04-21T09:00:08.210Z',
+          'votes': 19
+        },
+        {
+          '_id': '5530fb730857b80300388c70',
+          '__v': 0,
+          'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/RE365097472def4dc100312aab9a926eb7',
+          'updated': '2015-04-20T22:26:20.372Z',
+          'votes': 19
+        },
+        {
+          '_id': '5530fe0de9bf8c03009287c0',
+          '__v': 0,
+          'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/RE68b60e1ce417e871433673ad9326d182',
+          'updated': '2015-04-20T22:26:16.369Z',
+          'votes': 13
+        },
+        {
+          '_id': '553190a6e9bf8c03009287c4',
+          '__v': 0,
+          'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/RE3e23ce1d51648a797a3969e02c364b19',
+          'updated': '2015-04-21T09:02:31.344Z',
+          'votes': 16
+        },
+        {
+          '_id': '5536470e3856740300195fe2',
+          '__v': 0,
+          'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/RE6dc1e9f0b2a7f9bb1a23112d83771398',
+          'updated': '2015-04-21T12:48:14.181Z',
+          'votes': 18
+        },
+        {
+          '_id': '5546a1550204a403006f59bc',
+          '__v': 0,
+          'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/RE78af3e674e0fc645ef1d2260b1fea7f3',
+          'updated': '2015-05-03T22:29:41.628Z',
+          'votes': 17
+        }
+      ], 'updated', 'desc')
+    }
+  }
 
-    var posts = postsArray.map(
+  render() {
+    var posts = this.state.data.map(
       (post, index) => (
         <PostComponent
           key = {index}
