5f634fd6b806e66cf3eb9a0ea456c178334ff3f8
Mike Mitchell <mikemitchell@Mikes-MacBook-Air.local>
2016-07-10 13:43:56 +0100

    Basic posts

---
+++ src/components/PostComponent.js
@@ -0,0 +1,71 @@
+'use strict';
+
+import React from 'react';
+import Moment from 'moment';
+import Wavesurfer from 'react-wavesurfer';
+
+require('styles/Post.scss');
+
+class PostComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false
+    }
+
+    this.handleTogglePlay = this.handleTogglePlay.bind(this);
+  }
+
+  handleTogglePlay() {
+    this.setState({
+      playing: !this.state.playing
+    });
+  }
+
+  render() {
+    return (
+      <li className="post-component">
+        <button className="post-component__play-button" onClick={this.handleTogglePlay.bind(this)} >
+          <svg width="53px" height="53px" viewBox="0 0 53 53">
+            <g id="-" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" >
+              <g id="Desktop"  transform="translate(-496.000000, -469.000000)" fill="#333333">
+                <g id="Audio-Post"  transform="translate(422.000000, 469.000000)">
+                  <g id="Play-Button" transform="translate(74.000000, 0.000000)" >
+                    <path d="M26.5,53 C41.1355459,53 53,41.1355459 53,26.5 C53,11.8644541 41.1355459,0 26.5,0 C11.8644541,0 0,11.8644541 0,26.5 C0,41.1355459 11.8644541,53 26.5,53 Z M38,26.6726471 L20.1125,37 L20.1125,16.3452941 L38,26.6726471 Z" id="Oval-1"></path>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
+        </button>
+        <div className="post-component__audio">
+          <Wavesurfer
+            audioFile = { this.props.audio }
+            playing={ this.state.playing }
+            options = {
+              {
+                barWidth: 2,
+                progressColor: '#c6b266',
+                cursorWidth: 0,
+                waveColor: '#eeeeee',
+                height: '53'
+              }
+            }
+          />
+        </div>
+        <div className="post-component__meta">
+          Posted { Moment(this.props.date).fromNow() }
+        </div>
+      </li>
+    );
+  }
+}
+
+PostComponent.displayName = 'PostComponent';
+
+// Uncomment properties you need
+// PostComponent.propTypes = {};
+// PostComponent.defaultProps = {};
+
+export default PostComponent;
--- src/components/PostListComponent.js
+++ src/components/PostListComponent.js
@@ -2,21 +2,108 @@
 
 import React from 'react';
 import PostListFilterComponent from 'components/PostListFilterComponent';
+import PostComponent from 'components/PostComponent';
 
 require('styles/PostList.scss');
 
 class PostListComponent extends React.Component {
   render() {
+    var postsArray = [
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': 2,
+        'updated': 1429566400826,
+        '__v': 0
+      },
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': 2,
+        'updated': 1429566402608,
+        '__v': 0
+      },
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': 4,
+        'updated': 1429566409761,
+        '__v': 0
+      },
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': 1,
+        'updated': 1429566452774,
+        '__v': 0
+      },
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': 0,
+        'updated': 1429566465165,
+        '__v': 0
+      },
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': 2,
+        'updated': 1429566467349,
+        '__v': 0
+      },
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': -2,
+        'updated': 1429566469165,
+        '__v': 0
+      },
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': -2,
+        'updated': 1429566474210,
+        '__v': 0
+      },
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': -2,
+        'updated': 1429566476493,
+        '__v': 0
+      },
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': 10,
+        'updated': 1429566480274,
+        '__v': 0
+      },
+      {
+        'recording_url': 'http://api.twilio.com/2010-04-01/Accounts/AC2ec2a27cc56241e89887e63de463f7bb/Recordings/REb45c5ba936f3980c9e5f749537163e35',
+        'from': 'test',
+        'votes': 14,
+        'updated': 1429566482417,
+        '__v': 0
+      }
+    ];
+
+    var posts = postsArray.map(
+      (post, index) => (
+        <PostComponent
+          key = {index}
+          audio = {post.recording_url}
+          votes = {post.votes}
+          date = {post.updated}
+        />
+      )
+    )
+
     return (
       <div className="postlist-component">
         <PostListFilterComponent filterTitles={['Latest', 'Best', 'Worst']} />
         <ul className="postlist-component__list">
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-          <li>Item 4</li>
-          <li>Item 5</li>
-          <li>Item 6</li>
+          { posts }
         </ul>
       </div>
     );
